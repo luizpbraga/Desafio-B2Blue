ef6847e2db83ffbaea3bc42fd218b6e3c768dcd1
Luiz Alberto Portilho Braga <portilho.braga@gmail.com>
2025-04-10 12:27:04 +0000

    backend/feat: Add views templates

--- backend/storage/views.py
+++ backend/storage/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import Station, StationHistory
+from .serializers import StationSerializer, StationHistorySerializer
 
-# Create your views here.
+class StationViewSet(viewsets.ModelViewSet):
+    queryset = Station.objects.all()
+    serializer_class = StationSerializer
+
+    def perform_create(self, serializer):
+        """Criar uma nova estação e registrar no histórico"""
+        #TODO
+
+    def perform_update(self, serializer):
+        """Atualizar o volume da estação e verificar se precisa solicitar coleta"""
+        # TODO
+
+class StationHistoryViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = StationHistory.objects.all()
+    serializer_class = StationHistorySerializer
+    
+    def get_queryset(self):
+        """Permite filtrar o histórico por estação"""
+        # TODO
